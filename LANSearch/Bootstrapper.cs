447708a061cb0ef81e5b537336ae85aabfd474d4
Hiiru <hiiru@rulez.ch>
2014-10-05 21:29:44 +0200

    Added Scriptbundeling

--- LANSearch/Bootstrapper.cs
+++ LANSearch/Bootstrapper.cs
@@ -5,6 +5,7 @@ using Nancy.Bootstrapper;
 using Nancy.Conventions;
 using Nancy.Diagnostics;
 using Nancy.TinyIoc;
+using SquishIt.Framework;
 
 namespace LANSearch
 {
@@ -25,6 +26,23 @@ namespace LANSearch
             FormsAuthentication.Enable(pipelines, formsAuthenticationConfiguration);
             StaticConfiguration.DisableErrorTraces = false;
             Nancy.Security.Csrf.Enable(pipelines);
+
+            Bundle.Css()
+                .Add("~/Content/bootstrap.css")
+                .Add("~/Content/bootstrap-theme.css")
+                .Add("~/Content/bootstrap-dialog.css")
+                .Add("~/Content/lansearch.css")
+                .ForceRelease()
+                .AsCached("css", "/static/css");
+
+            Bundle.JavaScript()
+                .Add("~/Scripts/jquery-1.9.0.js")
+                .Add("~/Scripts/jquery.signalR-2.1.2.js")
+                .Add("~/Scripts/bootstrap.js")
+                .Add("~/Scripts/bootstrap-dialog.js")
+                .Add("~/Scripts/lansearch.js")
+                .ForceRelease()
+                .AsCached("js", "/static/js");
         }
 
         protected override DiagnosticsConfiguration DiagnosticsConfiguration
---
+++ LANSearch/Modules/StaticModule.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+using SquishIt.Framework;
+using System.IO;
+using System.Text;
+
+namespace LANSearch.Modules
+{
+    public class StaticModule : NancyModule
+    {
+        public StaticModule()
+            : base("/static")
+        {
+            Get["css"] = x =>
+            {
+                return CreateResponse(Bundle.Css().RenderCached("css"), Configuration.Instance.CssMimeType);
+            };
+            Get["js"] = x =>
+            {
+                return CreateResponse(Bundle.JavaScript().RenderCached("js"), Configuration.Instance.CssMimeType);
+            };
+        }
+
+        private Response CreateResponse(string content, string contentType)
+        {
+            return Response
+                .FromStream(() => new MemoryStream(Encoding.UTF8.GetBytes(content)), contentType)
+                .WithHeader("Cache-Control", "max-age=5");
+        }
+    }
+}
